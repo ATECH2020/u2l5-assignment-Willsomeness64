c9c580877c54e189e929d57a9ec2960a2ad84e53
William Giraldo <william.229937@nv.ccsd.net>
2020-09-26 06:25:02 +0000

    Saving this branch incase something bad happens:
    Initialized 4 Int variables, asked the user for latitude/longitude inputs, created two GeoLocation classes.

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,27 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        // "I still don't know what I'm doing" - AWOLNATION 
+      
+      //initializing my variables
+     int startLatitude;
+     int startlongitude;
+     int endLaditude;
+     int endLongitude;
+
+     //Asking the user for inputs for my variables
+     System.out.println("Enter the latitude of the starting location:");
+     startLatitude = in.nextInt();
+     System.out.println("Enter the longitude of the starting location: ");
+     startlongitude = in.nextInt();
+     System.out.println("Enter the latitude of the ending location: ");
+     endLaditude = in.nextInt();
+     System.out.println("Enter the longitude of the ending location: ");
+     endLongitude = in.nextInt(); 
+
+    //creating two GeoLocation objects, going to try to use these two objects to calculate the distance. 
+        GeoLocation startLoc = new GeoLocation(startLatitude,startlongitude);
+        GeoLocation endLoc = new GeoLocation(startLatitude,startlongitude);
+
     }
 }
